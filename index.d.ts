944a3780e581e8094642b084888225d102b40ac1
Mike Hansen <mike@mkhnsn.com>
2025-11-02 20:10:28 -0800

    expand compatibility with other environments

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface KeyProps {
+  children: React.ReactNode;
+}
+export declare const Key: React.FC<KeyProps>;
+
+export interface KeyComboProps {
+  combo?: string;
+  render?: (keys: string[]) => React.ReactNode;
+}
+export declare const KeyCombo: React.FC<KeyComboProps>;
+
+export declare function useKeyCombo(input: string): string[];
