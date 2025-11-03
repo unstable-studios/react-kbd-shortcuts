2bd1beaf6f9891be5649c772f257e3a7fcac9156
Mike Hansen <mike@mkhnsn.com>
2025-11-03 09:13:09 -0800

    fix gitignore for lockfile

--- examples/sandbox/vite.config.ts
+++ examples/sandbox/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "react-kbd-shortcuts": path.resolve(
