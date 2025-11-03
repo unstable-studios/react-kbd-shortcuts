622e075289e72d72873a31265f4452d477f9f19b
Mike Hansen <mike@mkhnsn.com>
2025-11-03 08:56:08 -0800

    clean up gitignore

--- packages/react-kbd-shortcuts/dist/hooks/useKeyCombo.js
+++
@@ -1,8 +0,0 @@
-import { parseKeyCombo } from '../utils/parseKeyCombo.js';
-
-function useKeyCombo(input, useSymbols = false) {
-  return parseKeyCombo(input, useSymbols);
-}
-
-export { useKeyCombo };
-//# sourceMappingURL=useKeyCombo.js.map
